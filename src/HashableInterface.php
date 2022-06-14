e8d05c2207dc4af71c25d1da9697473a76b87e2f
TOVOHERY <tovoherypascal@gmail.com>
2022-06-14 23:35:06 +0300

    licence file

--- src/HashableInterface.php
+++ src/HashableInterface.php
@@ -1,6 +1,17 @@
 <?php
 namespace Dadapas\Ds;
 
+/**
+ * This file is part of the dadapas/ds library
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @copyright Copyright (c) TOVOHERY Z. Pascal <tovoherypascal@gmail.com>
+ * @license http://opensource.org/licenses/MIT MIT 
+*/ 
+
+
 interface HashableInterface
 {
 	/**
--- src/Nodes/LinkListNode.php
+++ src/Nodes/LinkListNode.php
@@ -1,6 +1,16 @@
 <?php
 namespace Dadapas\Ds\Nodes;
 
+/**
+ * This file is part of the dadapas/ds library
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @copyright Copyright (c) TOVOHERY Z. Pascal <tovoherypascal@gmail.com>
+ * @license http://opensource.org/licenses/MIT MIT 
+*/ 
+
 class LinkListNode
 {
 	public function __construct($data)
